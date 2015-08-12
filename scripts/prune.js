e8a3da7fa12bd7aca869f80436d9ba7ef587fc3e
Alan Plum <me@pluma.io>
2015-08-12 14:41:02 +0200

    Fixed prune script.

--- scripts/prune.js
+++ scripts/prune.js
@@ -1,14 +1,14 @@
-/*global require, applicationContext */
+/*global require */
 'use strict';
-var qb = require('aqb');
-var db = require('org/arangodb').db;
-var collection = applicationContext.collection('sessions');
-var expiryType = applicationContext.configuration.expiryType;
-var timeout = applicationContext.configuration.expiryDuration * 1000;
-var now = Date.now();
+const qb = require('aqb');
+const db = require('org/arangodb').db;
+const sessions = require('./sessions');
+const now = Date.now();
+
 module.exports = db._query(
-  qb.for('session').in(collection)
-  .filter(qb.get('session', qb(expiryType)).plus(timeout).lt(now))
-  .remove('session').in(collection)
+  qb.for('session').in(sessions.collection)
+  .filter('session.expiry')
+  .filter(qb.ref('session.expiry').lt(now))
+  .remove('session').in(sessions.collection)
   .returnOld('x')
 ).toArray();
